25a396b7314d6f20092b7fc0c350cf2d84d0eb88
Ed O'Connor <ed@oconnor.app>
2022-07-12 12:57:55 -0400

    fixed mongodb process.env

--- src/server.js
+++ src/server.js
@@ -12,10 +12,7 @@ const port = process.env.PORT || 5000;
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const uri = process.env.MONGODB_URI;
 
 // =============> MONGODB DEV Connection:
 
