c71e136e2901d452da5b6d8d8132f3b89e95896f
Ed O'Connor <ed@oconnor.app>
2022-07-12 15:34:52 -0400

    ugh

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const connection = require("./src/connection.js");
+const connection = require("./connection");
 const Article = require("../models/article");
 const articleRouter = require("../routes/articles");
 const methodOverride = require("method-override");
