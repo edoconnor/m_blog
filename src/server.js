c4b9cc2e252415b27558d0b168f62fa22d4a5714
Ed O'Connor <ed@oconnor.app>
2022-07-12 15:23:50 -0400

    edit connection.js

--- src/server.js
+++ src/server.js
@@ -15,6 +15,22 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+client.connect((err) => {
+  const collection = client.db("test").collection("articles");
+  // perform actions on the collection object
+  client.close();
+});
+
 app.get("/", async (req, res) => {
   const articles = await Article.find().sort({ createdAt: "desc" });
   res.render("articles/index", { articles: articles });
